e2becd9d7df8a73c46f133fcd72ece887e595abb
Luke <luke@bedthreads.com.au>
2021-09-23 10:01:02 +1000

    fixed full screen bug, shopify not removing fullscreen class

--- src/feature/autoFullScreen.js
+++ src/feature/autoFullScreen.js
@@ -8,4 +8,11 @@ let interval = setInterval(() => {
 
   // Add fullscreen class to body
   $("body").addClass("fullscreen-mode");
+
+  let $exitButton = $('.template-editor-titlebar .ui-breadcrumbs a')
+  $exitButton.on( "click", () => {
+    $("body").removeClass("fullscreen-mode");
+  });
+
+  console.log('full', $exitButton);
 }, 500);
