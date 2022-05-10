5810633e9c29fc02d011b5f7fa40d640c07bd07d
luke.chadwick <luke.chadwick@acidgreen.com.au>
2022-05-10 11:05:18 +1000

    remove notification editor now that it's native

--- src/feature/notificationEditor.js
+++
@@ -1,9 +0,0 @@
-loadScript = (path) => {
-  let s = document.createElement('script');
-  s.src = chrome.runtime.getURL(path);
-  (document.head || document.documentElement).appendChild(s);
-}
-
-// loadScript('thirdParty/ace.js');
-
-// loadScript('src/feature/notificationEditorInject.js')
