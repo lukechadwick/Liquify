053f60577ebce44519ab534dccedd52667c67f2c
luke.chadwick <luke.chadwick@acidgreen.com.au>
2022-05-10 11:06:05 +1000

    wide admin for notification editor

--- src/feature/wideAdmin.js
+++ src/feature/wideAdmin.js
@@ -7,23 +7,39 @@ chrome.storage.sync.get("wideAdmin", function (data) {
 
 applyWidth = () => {
   setInterval(() => {
+    // Most admin pages
     let mainContainer = document.querySelector('div[class^="Polaris-Frame__Content"] div[class^="Polaris-Page_"]')
     if (mainContainer){
       mainContainer.style.setProperty('max-width', 'unset', 'important');  
     }
 
+    // App page
     let appContainer = document.querySelector('._9u-Hv')
     if (appContainer){
       appContainer.style.setProperty('display', 'none', 'important')
     }
 
+    // Settings page
     let settingsContainer = document.querySelector('._1e0Kf')
     if (settingsContainer){
       settingsContainer.style.setProperty('width', 'unset', 'important');  
       settingsContainer.style.setProperty('width', '100%', 'important');  
-
     }
 
+    // Notification editor
+    let editorContainer = document.querySelector('.ilNbO')
+    let editor = document.querySelector('.Polaris-Page_yisnh')
+
+    if (editorContainer){
+      editorContainer.style.setProperty('max-width', '100%', 'important');  
+      editorContainer.style.setProperty('width', '100%', 'important');  
+    }
+    if (editor){
+      editor.style.setProperty('max-width', '100%', 'important');
+      editor.style.setProperty('width', '100%', 'important');  
+    }
+
+    // themes page
     let themeContainer = document.querySelector('.Polaris-Page_yisnh')
     if (themeContainer){
       themeContainer.style.setProperty('max-width', 'unset', 'important');  
