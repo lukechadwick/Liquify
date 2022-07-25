fde91ae1fb366c90f8cd5fb978008a5cfd96194c
luke.chadwick <luke.chadwick@acidgreen.com.au>
2022-07-25 20:21:51 +1000

    update themes

--- src/feature/_theme.js
+++ src/feature/_theme.js
@@ -2,7 +2,7 @@
   setTheme = (theme) => {
     let searchIntervalNew = setInterval(() => {
       let hasThemeIframe = document.querySelector(`[title="Online Store"]`)
-      if (!hasThemeIframe) return
+      if (!hasThemeIframe) return;
 
       let hasThemeContent = hasThemeIframe.contentDocument.querySelector('[data-diffy-attribute="sidebar"]')
       if (!hasThemeContent) return;
@@ -10,14 +10,15 @@
       clearInterval(searchIntervalNew);
 
       let path = chrome.extension.getURL(`src/themes/${theme}-new.css`);
-      let iframeTo = document.querySelector(`[title="Online Store"]`)
-      var cssLink = document.createElement("link") 
-      cssLink.href = path
-      cssLink.rel = "stylesheet"; 
-      cssLink.type = "text/css"; 
-
+      let iframeTo = document.querySelector(`[title="Online Store"]`);
+      var cssLink = document.createElement("link");
+      cssLink.href = path;
+      cssLink.rel = "stylesheet";
+      cssLink.type = "text/css";
       iframeTo.contentWindow.document.body.appendChild(cssLink);
-      iframeTo.contentWindow.document.body.querySelector('#app').firstElementChild.setAttribute('p-color-scheme', 'dark')
+
+      if (theme != 'common')
+        iframeTo.contentWindow.document.body.querySelector('#app').firstElementChild.setAttribute('p-color-scheme', 'dark');
     }, 500);
   }
 
@@ -27,32 +28,33 @@
       let chosenTheme = data.chosenTheme.toLowerCase() || "vscode";
       chosenTheme != "none" &&
         setTheme(chosenTheme);
+      setTheme('common');
     });
   }
-  
+
   // Detect page change
   let previousUrl = '';
   const observer = new MutationObserver(() => {
     if (location.href !== previousUrl) {
-        previousUrl = location.href;
-        if (location.href.includes('/themes/') && !location.href.includes('editor')) {
-          getTheme();
-          try {
-            let iFrame = document.querySelector(`[title="Online Store"]`)
-            chrome.storage.sync.get("chosenTheme", function (data) {
-              data.chosenTheme.toLowerCase() != "none" &&
+      previousUrl = location.href;
+      if (location.href.includes('/themes/') && !location.href.includes('editor')) {
+        getTheme();
+        try {
+          let iFrame = document.querySelector(`[title="Online Store"]`)
+          chrome.storage.sync.get("chosenTheme", function (data) {
+            data.chosenTheme.toLowerCase() != "none" &&
               iFrame && iFrame.contentWindow.document.body.querySelector('#app').firstElementChild.setAttribute('p-color-scheme', 'dark')
-            });
-          } catch (error) {}
-        } else {
-          try {
-            let iFrame = document.querySelector(`[title="Online Store"]`)      
-            iFrame && iFrame.contentWindow.document.body.querySelector('#app').firstElementChild.setAttribute('p-color-scheme', 'light')  
-          } catch (error) {}
-        }
+          });
+        } catch (error) { }
+      } else {
+        try {
+          let iFrame = document.querySelector(`[title="Online Store"]`)
+          iFrame && iFrame.contentWindow.document.body.querySelector('#app').firstElementChild.setAttribute('p-color-scheme', 'light')
+        } catch (error) { }
       }
+    }
   });
 
-  const config = {subtree: true, childList: true};
+  const config = { subtree: true, childList: true };
   observer.observe(document, config);
 })();
