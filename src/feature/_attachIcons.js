e435668504356425d4770b4bb66117bf9183264c
lukechadwick <llukechadwick@gmail.com>
2023-02-09 20:43:19 +1100

    search for both old and new editor elements

--- src/feature/_attachIcons.js
+++ src/feature/_attachIcons.js
@@ -3,55 +3,65 @@
     // Create setInterval to act as a listener until theme editor elements exist
     let iconIntervalListener = setInterval(() => {
       // Locate iframe and sidebar
-      let hasThemeIframe = document.querySelector(`main iframe`)
-      if (!hasThemeIframe) return;
+      let hasThemeIframe = document.querySelector(`main iframe`);
 
-      let hasSidebar = hasThemeIframe.contentDocument.querySelector('[data-diffy-attribute="sidebar"]')
+      let frameDocument =
+        (hasThemeIframe && hasThemeIframe.contentDocument) || document;
+
+      let hasSidebar =
+        (hasThemeIframe &&
+          hasThemeIframe.contentDocument &&
+          hasThemeIframe.contentDocument.querySelector(
+            '[data-diffy-attribute="sidebar"]'
+          )) ||
+        document.querySelector('[data-diffy-attribute="sidebar"]');
       if (!hasSidebar) return;
 
       // Element found, clear interval
       clearInterval(iconIntervalListener);
 
       // Inject icon CSS stylesheet into iframe
-      let path = chrome.runtime.getURL('src/themes/icons.css');
-      let iframeTo = document.querySelector(`main iframe`)
-      var cssLink = document.createElement("link") 
-      cssLink.href = path
-      cssLink .rel = "stylesheet"; 
-      cssLink .type = "text/css"; 
-      iframeTo.contentWindow.document.body.appendChild(cssLink);
+      let path = chrome.runtime.getURL("src/themes/icons.css");
+      var cssLink = document.createElement("link");
+      cssLink.href = path;
+      cssLink.rel = "stylesheet";
+      cssLink.type = "text/css";
+
+      frameDocument.body.appendChild(cssLink);
 
       setInterval(() => {
         // Locate iframe and file list
-        let iFrameDocument = document.querySelector(`main iframe`);
-        if (!iFrameDocument) return
 
-        let fileList = iFrameDocument.contentDocument.querySelectorAll('[aria-label="File picker"] li[data-diffy-attribute^="fileName-"]');
+        let fileList = frameDocument.querySelectorAll(
+          '[aria-label="File picker"] li[data-diffy-attribute^="fileName-"]'
+        );
         // Apply icon classes to file elements
         for (let file of fileList) {
           let fileIcon = file.querySelector("svg");
 
           switch (true) {
-            case file.getAttribute('data-diffy-attribute').includes(".json"):
+            case file.getAttribute("data-diffy-attribute").includes(".json"):
               fileIcon.style.fill = "orange";
               fileIcon.style.display = "inline";
               break;
-            case file.getAttribute('data-diffy-attribute').includes(".js") ||
-              file.getAttribute('data-diffy-attribute').includes(".js.liquid"):
+            case file.getAttribute("data-diffy-attribute").includes(".js") ||
+              file.getAttribute("data-diffy-attribute").includes(".js.liquid"):
               fileIcon.parentNode.classList.add("is-js");
               fileIcon.style.display = "none";
               break;
-            case file.getAttribute('data-diffy-attribute').includes(".scss") ||
-              file.getAttribute('data-diffy-attribute').includes(".scss.liquid"):
+            case file.getAttribute("data-diffy-attribute").includes(".scss") ||
+              file
+                .getAttribute("data-diffy-attribute")
+                .includes(".scss.liquid"):
               fileIcon.parentNode.classList.add("is-scss");
               fileIcon.style.display = "none";
               break;
-            case file.getAttribute('data-diffy-attribute').includes(".css") ||
-              file.getAttribute('data-diffy-attribute').includes(".css.liquid"):
+            case file.getAttribute("data-diffy-attribute").includes(".css") ||
+              file.getAttribute("data-diffy-attribute").includes(".css.liquid"):
               fileIcon.parentNode.classList.add("is-css");
               fileIcon.style.display = "none";
               break;
-            case file.getAttribute('data-diffy-attribute').includes(".liquid"):
+            case file.getAttribute("data-diffy-attribute").includes(".liquid"):
               fileIcon.style.fill = "rgb(140, 190, 70)";
               fileIcon.style.display = "inline";
               break;
@@ -60,49 +70,50 @@
       }, 500);
 
       setInterval(() => {
-        // Locate iframe and tab list
-        let iFrameDocument = document.querySelector(`main iframe`);
-        if (!iFrameDocument) return;
-
-        let tabList = iFrameDocument.contentDocument.querySelectorAll('[data-diffy-attribute="tablist"] [data-diffy-attribute]');
+        let tabList = frameDocument.querySelectorAll(
+          '[data-diffy-attribute="tablist"] [data-diffy-attribute]'
+        );
 
         // Apply icon classes tabs
         for (let tab of tabList) {
           switch (true) {
-            case tab.getAttribute('data-diffy-attribute').includes(".json"):
+            case tab.getAttribute("data-diffy-attribute").includes(".json"):
               tab.classList.add("is-json");
               break;
-            case tab.getAttribute('data-diffy-attribute').includes(".js") ||
-              tab.getAttribute('data-diffy-attribute').includes(".js.liquid"):
+            case tab.getAttribute("data-diffy-attribute").includes(".js") ||
+              tab.getAttribute("data-diffy-attribute").includes(".js.liquid"):
               tab.classList.add("is-js");
               break;
-            case tab.getAttribute('data-diffy-attribute').includes(".scss") ||
-              tab.getAttribute('data-diffy-attribute').includes(".scss.liquid"):
+            case tab.getAttribute("data-diffy-attribute").includes(".scss") ||
+              tab.getAttribute("data-diffy-attribute").includes(".scss.liquid"):
               tab.classList.add("is-scss");
               break;
-            case tab.getAttribute('data-diffy-attribute').includes(".css") ||
-              tab.getAttribute('data-diffy-attribute').includes(".css.liquid"):
+            case tab.getAttribute("data-diffy-attribute").includes(".css") ||
+              tab.getAttribute("data-diffy-attribute").includes(".css.liquid"):
               tab.classList.add("is-css");
               break;
-            case tab.getAttribute('data-diffy-attribute').includes(".liquid"):
+            case tab.getAttribute("data-diffy-attribute").includes(".liquid"):
               tab.classList.add("is-liquid");
               break;
           }
         }
       }, 500);
     }, 500);
-  }
+  };
 
   // Detect page change
-  let previousUrl = '';
+  let previousUrl = "";
   const observer = new MutationObserver(() => {
     if (location.href !== previousUrl) {
-        previousUrl = location.href;
-        if (location.href.includes('/themes/') && !location.href.includes('editor'))
-          attachIcons();
-      }
+      previousUrl = location.href;
+      if (
+        location.href.includes("/themes/") &&
+        !location.href.includes("editor")
+      )
+        attachIcons();
+    }
   });
 
-  const config = {subtree: true, childList: true};
+  const config = { subtree: true, childList: true };
   observer.observe(document, config);
 })();
--- src/feature/_tabBehaviour.js
+++ src/feature/_tabBehaviour.js
@@ -3,70 +3,86 @@
     // Create setInterval to act as a listener until theme editor elements exist
     let tabIntervalListener = setInterval(() => {
       // Locate iframe and sidebar
-      let hasThemeIframe = document.querySelector(`main iframe`)
-      if (!hasThemeIframe) return;
+      let hasThemeIframe = document.querySelector(`main iframe`);
 
-      let hasThemeContent = hasThemeIframe.contentDocument.querySelector('[data-diffy-attribute="sidebar"]')
+      let frameDocument =
+        (hasThemeIframe && hasThemeIframe.contentDocument) || document;
+
+      let hasThemeContent =
+        (hasThemeIframe &&
+          hasThemeIframe.contentDocument &&
+          hasThemeIframe.contentDocument.querySelector(
+            '[data-diffy-attribute="sidebar"]'
+          )) ||
+        document.querySelector('[data-diffy-attribute="sidebar"]');
       if (!hasThemeContent) return;
 
       // Element found, clear interval
       clearInterval(tabIntervalListener);
 
       // Drag and drop tabs
-      dragDropTabs(hasThemeIframe)
+      dragDropTabs(frameDocument);
 
       // If stacked tabs is enabled
-      if (theme == 'tabs') {
+      if (theme == "tabs") {
         let path = chrome.runtime.getURL(`src/themes/${theme}.css`);
-        let iframeTo = document.querySelector(`main iframe`);
         var cssLink = document.createElement("link");
         cssLink.href = path;
         cssLink.rel = "stylesheet";
         cssLink.type = "text/css";
-        iframeTo.contentWindow.document.body.appendChild(cssLink);
+        frameDocument.body.appendChild(cssLink);
       }
     }, 500);
-  }
+  };
 
   getTabs = () => {
     chrome.storage.sync.get("tabBehaviour", function (data) {
-      data.tabBehaviour == "Stack" ?
-        setTabs('tabs') : setTabs();
+      data.tabBehaviour == "Stack" ? setTabs("tabs") : setTabs();
     });
-  }
+  };
 
   dragDropTabs = (frame) => {
     setInterval(() => {
       // If no theme iframe content
-      if (!frame.contentDocument) return
+      if (!frame) return;
 
       // Get all editor tabs and set draggable attribute
-      let tabList = frame.contentDocument.querySelectorAll('[data-diffy-attribute="tablist"] [role="presentation"]')
-      tabList.forEach(tab => {
-        tab.setAttribute('draggable', true)
-      })
+      let tabList = frame.querySelectorAll(
+        '[data-diffy-attribute="tablist"] [role="presentation"]'
+      );
+      tabList.forEach((tab) => {
+        tab.setAttribute("draggable", true);
+      });
 
       // Find all tabs that don't have the hasEvent class
-      frame.contentDocument.querySelectorAll('[draggable="true"]:not(.hasEvent)').forEach(dragg => {
-        dragg.addEventListener('dragstart', e => {
+      frame
+        .querySelectorAll('[draggable="true"]:not(.hasEvent)')
+        .forEach((dragg) => {
+          dragg.addEventListener("dragstart", (e) => {
             // On drag
             e.target.style.opacity = 0.2;
-        })
-        dragg.addEventListener('dragover', e => {
-          // Disable float-back animation
-          e.preventDefault()
-        })
-        dragg.addEventListener('dragend', e => {
+          });
+          dragg.addEventListener("dragover", (e) => {
+            // Disable float-back animation
+            e.preventDefault();
+          });
+          dragg.addEventListener("dragend", (e) => {
             // On drop
             e.preventDefault();
             let targetX = e.x;
             let targetY = e.y;
-            let moveTo = frame.contentDocument.elementFromPoint(targetX, targetY).closest('[role="presentation"]');
+            let moveTo = frame
+              .elementFromPoint(targetX, targetY)
+              .closest('[role="presentation"]');
 
             // If tab is being dragged to a tab with a higher index, place after, otherwise place before
             if (moveTo) {
-              let indexSelected = Array.from(moveTo.parentNode.children).indexOf(moveTo)
-              let indexTarget = Array.from(dragg.parentNode.children).indexOf(dragg)
+              let indexSelected = Array.from(
+                moveTo.parentNode.children
+              ).indexOf(moveTo);
+              let indexTarget = Array.from(dragg.parentNode.children).indexOf(
+                dragg
+              );
 
               if (indexTarget < indexSelected) {
                 moveTo.parentNode.insertBefore(dragg, moveTo.nextSibling);
@@ -75,24 +91,27 @@
               }
             }
             e.target.style.opacity = 1;
-        })
-        // Add hasEvent class to prevent duplicate listeners
-        dragg.classList.add('hasEvent')
-      })
+          });
+          // Add hasEvent class to prevent duplicate listeners
+          dragg.classList.add("hasEvent");
+        });
     }, 500);
-  }
+  };
 
   // Detect page change
-  let previousUrl = '';
-  const observer = new MutationObserver(() => {
+  let previousUrl = "";
+  const observer2 = new MutationObserver(() => {
     if (location.href !== previousUrl) {
       previousUrl = location.href;
-      if (location.href.includes('/themes/') && !location.href.includes('editor')) {
+      if (
+        location.href.includes("/themes/") &&
+        !location.href.includes("editor")
+      ) {
         getTabs();
       }
     }
   });
 
   const config = { subtree: true, childList: true };
-  observer.observe(document, config);
+  observer2.observe(document, config);
 })();
