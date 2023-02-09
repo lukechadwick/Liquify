19703baa409923290bbc9eafaf71f34ceb46132a
luke.chadwick <luke.chadwick@acidgreen.com.au>
2023-02-09 20:12:44 +1100

    connect search to talk to the assetExtractor, search for both old and new editor elements

--- src/feature/_search.js
+++ src/feature/_search.js
@@ -1,101 +1,95 @@
 (() => {
   startSearch = () => {
+    if (!window.SEARCH_INJECTED_FLAG) {
+      window.SEARCH_INJECTED_FLAG = true;
+      return false;
+    }
+
     // Create setInterval to act as a listener until theme editor elements exist
     let searchIntervalListener = setInterval(() => {
       // Locate iframe and file search element
-      let hasThemeIframe = document.querySelector(`main iframe`)
-      if (!hasThemeIframe) return
+      let hasThemeIframe = document.querySelector(`main iframe`);
 
-      let frameContent = hasThemeIframe.contentDocument.querySelector('[placeholder="Search files..."]')
+      let frameDocument =
+        (hasThemeIframe && hasThemeIframe.contentDocument) || document;
+
+      let frameContent =
+        (hasThemeIframe &&
+          hasThemeIframe.contentDocument &&
+          hasThemeIframe.contentDocument.querySelector(
+            '[placeholder="Search files..."]'
+          )) ||
+        document.querySelector('[placeholder="Search files..."]');
       if (!frameContent) return;
 
       // Element found, clear interval
       clearInterval(searchIntervalListener);
 
+      chrome.runtime.onMessage.addListener(function (request, sender) {
+        let parsed = JSON.parse(request.data);
+        // Get asset list
+        if (request.data) getAssets(frameContent, parsed);
+      });
+
       // Set loading message
       let searchInput = frameContent;
       searchInput.setAttribute("placeholder", "Enhanced search loading...");
-      searchInput.closest('[data-diffy-attribute="search"]').classList.add("search-loading");
+      searchInput
+        .closest('[data-diffy-attribute="search"]')
+        .classList.add("search-loading");
       searchInput.disabled = true;
-      
-      getAssets = (searchInput) => {
-        // Get Asset list from server
-        fetch(
-          window.location.href.split("?")[0] + "/assets.json",
-        )
-        .then((response) => response.json())
-        .then(function (data) {
-          // Filter out assets that aren't in the allowed list
-          let allowedExtensions = [".liquid", ".js", ".css", ".scss"];
-          let filteredResponse = data.assets.filter((word) =>
-            allowedExtensions.some((v) => word.key.includes(v))
-          );
 
-          getAssetData(filteredResponse, searchInput);
-        });
+      getAssets = (searchInput, parsed) => {
+        getAssetData(searchInput, parsed);
       };
 
-      getUserAsync = async (filteredResponse) => {
-        const result = []
-        // Batch request all valid asset files asynchronously, then add them to the results array 
-        const datas = 
-          filteredResponse.map(asset => {
-            try {
-              return fetch(
-                  window.location.href.split("?")[0] +
-                  "/assets.json?asset[key]=" +
-                  asset.key
-              )
-              .then((response) => response.json())
-              .then(item => {
-                result.push(item)
-              })
-            } catch (error) {
-              console.log('asset fetch fail: ', asset, error);
-            }
-          })
-        return Promise.all(datas).then(() => result)
-      }
-
-      getAssetData = (filteredResponse, frameContent) => {
-        // Fetch asset data
-        getUserAsync(filteredResponse)
-          .then(data => createSearchField(data, frameContent)); 
+      getAssetData = (frameContent, parsed) => {
+        createSearchField(parsed, frameContent);
       };
 
       createSearchField = (assetArray, searchElement) => {
-        searchElement.closest('[data-diffy-attribute="search"]').classList.remove("search-loading");
-        
+        searchElement
+          .closest('[data-diffy-attribute="search"]')
+          .classList.remove("search-loading");
+
         // Clone and replace search element to remove event listeners
         let searchParent = searchElement.parentNode;
-        const newItem = document.createElement('div');
-        newItem.innerHTML = '<input id="liquify-search" placeholder="Enhanced search loading..." autocomplete="off" class="Polaris-TextField__Input_30ock Polaris-TextField__Input--hasClearButton_15k6h search-loading" type="search" aria-labelledby="PolarisTextField2Label PolarisTextField2-Prefix" aria-invalid="false" value="">';
-        searchElement.parentNode.replaceChild(newItem.firstElementChild, searchElement);
+        const newItem = document.createElement("div");
+        newItem.innerHTML =
+          '<input id="liquify-search" placeholder="Enhanced search loading..." autocomplete="off" class="Polaris-TextField__Input_30ock Polaris-TextField__Input--hasClearButton_15k6h search-loading" type="search" aria-labelledby="PolarisTextField2Label PolarisTextField2-Prefix" aria-invalid="false" value="">';
+        searchElement.parentNode.replaceChild(
+          newItem.firstElementChild,
+          searchElement
+        );
 
         // Set properties of new search
-        let searchInput = searchParent.querySelector('#liquify-search')
-        searchInput.setAttribute("placeholder", "Search filename / contents...");
+        let searchInput = searchParent.querySelector("#liquify-search");
+        searchInput.setAttribute(
+          "placeholder",
+          "Search filename / contents..."
+        );
         searchInput.classList.remove("search-loading");
-        
+
         // On type event listener
-        searchInput.addEventListener('input', e => {
+        searchInput.addEventListener("input", (e) => {
           // Open all folders so all files render
-          let iFrameDocument = document.querySelector(`main iframe`).contentWindow.document.body;
-          let collapsibles = iFrameDocument.querySelectorAll('[id^=Collapsible-folder-][aria-hidden="true"]')
-          collapsibles.forEach(collapsible => {
-            collapsible.parentElement.querySelector('button').click()
+          let collapsibles = frameDocument.querySelectorAll(
+            '[id^=Collapsible-folder-][aria-hidden="true"]'
+          );
+          collapsibles.forEach((collapsible) => {
+            collapsible.parentElement.querySelector("button").click();
           });
 
           let searchTerm = e.target.value;
 
           // Find matches in asset files
-          let filtered = assetArray.filter((obj) => 
+          let filtered = assetArray.filter((obj) =>
             Object.values(obj).some((asset) => {
-              if ('value' in asset) {
-                return asset.value.includes(searchTerm)
+              if ("value" in asset) {
+                return asset.value.includes(searchTerm);
               } else {
-                console.log('asset failed', asset);
-                return false
+                console.log("asset failed", asset);
+                return false;
               }
             })
           );
@@ -103,16 +97,17 @@
           // Create list of matched files
           let filteredFiles = filtered.map((item) => (item = item.asset.key));
           filterFileList(filteredFiles, searchTerm);
-        })
+        });
       };
 
       filterFileList = (fileList, searchTerm) => {
-        let iFrameDocument = document.querySelector(`main iframe`).contentWindow.document.body;
-        let assetList = iFrameDocument.querySelectorAll('[aria-label="File picker"] li[data-diffy-attribute^="fileName-"]');
+        let assetList = frameDocument.querySelectorAll(
+          '[aria-label="File picker"] li[data-diffy-attribute^="fileName-"]'
+        );
 
         for (let item of assetList) {
           let assetKey = item.getAttribute("data-diffy-attribute");
-          assetKey = assetKey.slice(assetKey.indexOf('-') + 1);
+          assetKey = assetKey.slice(assetKey.indexOf("-") + 1);
 
           // Show file if filename or contents match search term, else hide
           if (assetKey) {
@@ -124,7 +119,9 @@
               item.style.display = "block";
               // Open asset folder if collapsed
               try {
-                item.parentNode.parentNode.parentNode.classList.remove("is-collapsed");
+                item.parentNode.parentNode.parentNode.classList.remove(
+                  "is-collapsed"
+                );
               } catch (error) {}
             } else {
               item.style.display = "none";
@@ -133,23 +130,25 @@
         }
       };
 
-      // Get asset list
-      getAssets(frameContent);
+      chrome.runtime.sendMessage({ searchReady: true });
     }, 500);
-  }
+  };
 
   // On load begin search
   startSearch();
 
   // Detect page change
-  let previousUrl = '';
-  const observer = new MutationObserver(() => {
+  let previousUrl = "";
+  const assetObserver = new MutationObserver(() => {
     if (location.href !== previousUrl) {
-        previousUrl = location.href;
-        if (location.href.includes('/themes/') && !location.href.includes('editor')) 
-          startSearch();
-      }
+      previousUrl = location.href;
+      if (
+        location.href.includes("/themes/") &&
+        !location.href.includes("editor")
+      )
+        startSearch();
+    }
   });
-  const config = {subtree: true, childList: true};
-  observer.observe(document, config);
+  const config = { subtree: true, childList: true };
+  assetObserver.observe(document, config);
 })();
